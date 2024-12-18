750636a429b13a5404dade4a9f17a79beda71bad
lisonnnnnnn <lison.olympie@edu.devinci.fr>
2024-12-18 16:15:08 +0100

    Update De.cs

--- Problème2024/De.cs
+++ Problème2024/De.cs
@@ -8,5 +8,36 @@ namespace Problème2024
 {
     internal class De
     {
+        private char[] sixFaces;
+        private char faceVisible;
+
+        public De(Random r)
+        {
+            sixFaces = new char[6];
+
+            for (int i = 0; i < 6; i++)
+            {
+                int indiceLettreAleatoire = r.Next(Alphabet.Possibilites.Count - 1);
+                sixFaces[i] = Alphabet.Possibilites[indiceLettreAleatoire];
+                Alphabet.Possibilites.RemoveAt(indiceLettreAleatoire);
+            }
+            Lance(r);
+        }
+        public char[] SixFaces
+        {
+            get { return sixFaces; }
+        }
+        public char FaceVisible
+        {
+            get { return faceVisible; }
+        }
+        public void Lance(Random r)
+        {
+            faceVisible = sixFaces[r.Next(5)];
+        }
+        public string toString()
+        {
+            return $" La face visible est {faceVisible} parmi les six lettres suivantes : {string.Join(" ", sixFaces)}";
+        }
     }
 }
